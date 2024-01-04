6f9bc3bbf283ae4ed7a8cd56aed8471201f34d6e
VincentG42 <goyetvincent@gmail.com>
2024-01-04 17:06:33 +0100

    avancement titles.php

--- pages/titles.php
+++ pages/titles.php
@@ -6,36 +6,36 @@ include_once('../process/connect_db.php');
 
 
 
-<div class="svg">
-<svg xmlns="http://www.w3.org/2000/svg" height="56" width="72"class="d-flex justify-content-center"viewBox="0 0 384 512">
-    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-    <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/>
-  </svg>
-</div>
-<audio id="title">
-  <source src="../mp3/MartijnSchmit-VacsInTheMorning.mp3"></source>
-  
-</audio>
 
-<div class='svg2'>
-<svg xmlns="http://www.w3.org/2000/svg" height="56" width="72" viewBox="0 0 320 512">
-  <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-<path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/>
-</svg>
-</div>
 
 <?php
 
 foreach($songs as $song){ ?>
-  <div class="songs_unit">
+  <div class="songs_unit row">
 
-  <img src="../img/<?= $song['pictures']?>">
-  <div>
-    <p><?= $song['title']?></p>
-    <p><?= $song['singer']?></p>
+    <img  class="minipic col-2" src="../img/<?= $song['pictures']?>">
+    <div class="col-6">
+        <p><?= $song['title']?></p>
+        <p><?= $song['singer']?></p>
+    </div>
+    <div class="col-2">
+    <div class="svg">
+      <svg xmlns="http://www.w3.org/2000/svg" height="20px" width="auto" viewBox="0 0 384 512">
+        <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+       <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/>
+      </svg>
+    </div>
+      <audio class="title">
+          <source src="../mp3/<?=$song['track']?>"></source>
+      </audio>
 
-  </div>
-  
+    <div class='svg2'>
+      <svg xmlns="http://www.w3.org/2000/svg" height="20px" width="auto" viewBox="0 0 320 512">
+        <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+        <path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/>
+      </svg>
+    </div>
+    </div>
 
     
 
--- process/treatment_titles.php
+++ process/treatment_titles.php
@@ -1,6 +1,6 @@
 <?php
 
-
+require_once('../process/connect_db.php');
 
 $request = $database->query("SELECT * FROM song");
     $songs = $request->fetchAll();
