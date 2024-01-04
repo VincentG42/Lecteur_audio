ebc94d14277e8eb3392cac717c42445247905761
Dev404 <jamesoncherry243@gmail.com>
2024-01-04 09:14:47 +0100

    spotify

---
+++ pages/connect.php
@@ -0,0 +1,60 @@
+<?php
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="d-flex clum"
+<div class="h-100 w-100">
+    <div class="container bg-light p-5 d-flex justify-content-center">
+        <form action="../process/treatment_connect.php" method="post">
+            <div class="mb-3 d-flex justify-content-center">
+                <label for="exampleInputEmail1" class="form-label">Enter votre pseudo : </label>
+                <input type="text" class="form" name="pseudo" id="exampleInputtext">
+            </div>
+            <button type="submit" class="btn btn-white">Valider</button>
+        </form>
+    </div>
+    </div>
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
