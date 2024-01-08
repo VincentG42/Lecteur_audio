8d2b032b3e627b621b6333e6f0ba64b0ca893035
VincentG42 <goyetvincent@gmail.com>
2024-01-08 15:58:45 +0100

    audio playlist ok

--- js/audioplayer.js
+++ js/audioplayer.js
@@ -1,19 +1,67 @@
-
 audioPlayer();
 
+console.log($("#audioplayer")[0]);
+console.log($("#playlist li"));
 
 function audioPlayer(){
     let currentSong =0
+    let audioPlayer = document.querySelector('#audioplayer')
     $("#audioplayer")[0].src =$("#playlist li a")[0];
     $("#audioplayer")[0].play();
     $("#playlist li a").click(function (e){
         e.preventDefault();
         $("#audioplayer")[0].src = this;
         $("#audioplayer")[0].play();
-        $("#playlist li").removeClass("current_song");
         currentSong =$(this).parent().index();
-        $(this).parent.addClass("current_song");    
+       
     })
+
+        audioPlayer.addEventListener("ended",function () {
+            currentSong++;
+            if (currentSong == $("#playlist li a").length)
+            currentSong = 0;
+            audioPlayer.src = $("#playlist li a")[currentSong].href;
+            audioPlayer.play(); 
+    });
+
+    let forward = document.querySelector('#forward')
+        forward.addEventListener("click",function () {
+            currentSong++;
+            if (currentSong == $("#playlist li a").length)
+            currentSong = 0;
+            audioPlayer.src = $("#playlist li a")[currentSong].href;
+            audioPlayer.play(); 
+    });
+
+    let backward = document.querySelector('#backward')
+    backward.addEventListener("click",function () {
+            currentSong--;           
+            audioPlayer.src = $("#playlist li a")[currentSong].href;
+            audioPlayer.play(); 
+    });
+
+    let currentIndex = $("#playlist li a").length;
+    function entierAleatoire(max){
+        indexRandom = Math.floor(Math.random() * (max - 1 + 1));
+        return  indexRandom
+    }
+    let shuffleNext= document.querySelector('#shuffle')
+    
+    shuffleNext.addEventListener("click",function () {       
+        
+        entierAleatoire(currentIndex)
+        audioPlayer.src = $("#playlist li a")[indexRandom].href;
+        audioPlayer.play(); 
+    });
     
 }
+
+
+
+    
+    
+    
+        
+     
+
 
--- pages/audioplayer_playlist.php
+++ pages/audioplayer_playlist.php
@@ -11,17 +11,22 @@ if (isset($_POST['playlist_id'])){
 
     $playlistToPLay = $request -> fetchAll();
 }
-var_dump($playlistToPLay);
+// var_dump($playlistToPLay);
 ?>
-<div class="audioplayer_wrapper">
-        <audio  src="" controls id="audioplayer">
+<div class="audioplayer_wrapper d-flex flex-column justify-content-center align-items-center mt-5">
+        <audio  src="" controls id="audioplayer" class="w-50 mb-3">
            test
         </audio>
-
-        <ul id="playlist">
+        <div class="col-12 d-flex justify-content-around w-50 mb-3">
+        <div id="onplay"></div>
+            <svg xmlns="http://www.w3.org/2000/svg" id="backward" height="2em" width="2em" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path opacity="1" fill="#1E3050" d="M459.5 440.6c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29V96c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4L288 214.3V256v41.7L459.5 440.6zM256 352V256 128 96c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4l-192 160C4.2 237.5 0 246.5 0 256s4.2 18.5 11.5 24.6l192 160c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29V352z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" id="shuffle" height="2em" width="2em" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path opacity="1" fill="#1E3050" d="M403.8 34.4c12-5 25.7-2.2 34.9 6.9l64 64c6 6 9.4 14.1 9.4 22.6s-3.4 16.6-9.4 22.6l-64 64c-9.2 9.2-22.9 11.9-34.9 6.9s-19.8-16.6-19.8-29.6V160H352c-10.1 0-19.6 4.7-25.6 12.8L284 229.3 244 176l31.2-41.6C293.3 110.2 321.8 96 352 96h32V64c0-12.9 7.8-24.6 19.8-29.6zM164 282.7L204 336l-31.2 41.6C154.7 401.8 126.2 416 96 416H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96c10.1 0 19.6-4.7 25.6-12.8L164 282.7zm274.6 188c-9.2 9.2-22.9 11.9-34.9 6.9s-19.8-16.6-19.8-29.6V416H352c-30.2 0-58.7-14.2-76.8-38.4L121.6 172.8c-6-8.1-15.5-12.8-25.6-12.8H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96c30.2 0 58.7 14.2 76.8 38.4L326.4 339.2c6 8.1 15.5 12.8 25.6 12.8h32V320c0-12.9 7.8-24.6 19.8-29.6s25.7-2.2 34.9 6.9l64 64c6 6 9.4 14.1 9.4 22.6s-3.4 16.6-9.4 22.6l-64 64z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" id="forward" height="2em" width="2em" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path opacity="1" fill="#1E3050" d="M52.5 440.6c-9.5 7.9-22.8 9.7-34.1 4.4S0 428.4 0 416V96C0 83.6 7.2 72.3 18.4 67s24.5-3.6 34.1 4.4L224 214.3V256v41.7L52.5 440.6zM256 352V256 128 96c0-12.4 7.2-23.7 18.4-29s24.5-3.6 34.1 4.4l192 160c7.3 6.1 11.5 15.1 11.5 24.6s-4.2 18.5-11.5 24.6l-192 160c-9.5 7.9-22.8 9.7-34.1 4.4s-18.4-16.6-18.4-29V352z"/></svg>    
+        </div>
+        <ul id="playlist" class="list-group w-50">
             <?php foreach ($playlistToPLay as $song){ ?>
-                <li class="current_song">
-                    <a href="../mp3/<?= $song['track']?>"> <img src="../img/<?= $song['pictures']?>" alt="" height="20px" width="auto"><?= $song['title']?> - <?= $song['singer']?></a>
+                <li class="playslist_row list-group-item py-2">
+                    <a href="../mp3/<?= $song['track']?>" class="link_playlist"> <img src="../img/<?= $song['pictures']?>" alt="" class="mx-5" height="50em" width="auto"><?= $song['title']?> - <?= $song['singer']?></a>
                 </li>
                 
             <?php } ?>
